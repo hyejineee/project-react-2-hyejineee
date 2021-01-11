26176c9ba599bc61a375649c0a4a1cdcd26c8dff
hyejineee <dev.hyejin04@gmail.com>
2021-01-11 22:05:07 +0900

    Change publicPath of file-loader
    도메인을 사용하기 위해 file-loader의 publicPath를 변경하였다.

--- src/presentational/SelectedOptions.jsx
+++ src/presentational/SelectedOptions.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
-import SelectedOption from 'presentational/SelectedOption';
-
 const Container = styled.div(({ theme }) => ({
   font: theme.typos.l,
   textAlign: 'center',
   color: theme.colors.main,
 }));
+
+const SelectedOption = styled.p({
+  display: 'inline-block',
+  padding: '0.3rem 1rem',
+  margin: '0.5rem',
+  borderRadius: '19px',
+  boxShadow: '-1px 3px 15px -2px rgba(165, 165, 165, 0.49)',
+  backgroundColor: '#ffffff',
+});
 
 export default function SelectedOptions({ selectedOptions }) {
   const {
@@ -17,17 +24,25 @@ export default function SelectedOptions({ selectedOptions }) {
 
   return (
     <Container>
-      <SelectedOption option={region} />
+      <SelectedOption>
+        {region}
+      </SelectedOption>
       {' '}
       에서
-      <SelectedOption option={level} />
+      <SelectedOption>
+        {level}
+      </SelectedOption>
       {' '}
       를 위한
       <br />
-      <SelectedOption option={season} />
+      <SelectedOption>
+        {season}
+      </SelectedOption>
       에 제격인
       {' '}
-      <SelectedOption option={activity} />
+      <SelectedOption>
+        {activity}
+      </SelectedOption>
       {' '}
       목적의 등산로입니다!
     </Container>
